9e70b89c1c74636e8aa9accb659701bbc4899b0d
MoUsaidh <usaidhsuhail@gmail.com>
2025-01-02 21:53:36 +0530

    update

---
+++ src/main/java/com/mo/tms/entity/User.java
@@ -0,0 +1,14 @@
+package com.mo.tms.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(schema = "user")
+@Entity
+public class User {
+
+
+
+}
